e831cb9e2e9e1acfdca811c6978e684b378d7c4b
user-metaterongo <paudelayush007@gmail.com>
2020-07-10 18:21:58 +0545

    changes

--- mainwindow.h
+++ mainwindow.h
@@ -22,7 +22,6 @@ public:
 private:
     void Action();
     void MenuItem();
-    //void createOwnerWindow();
 
 
 
@@ -36,23 +35,7 @@ private slots:
     void question();
 
     private:
-    /*//void makeReservation(int,int);
-    //void createWindow();
-    void createManagerWindow();
-    void createCustomerWindow();
-    //void bookWindow();
-    void createActions();
-    void createMenus();
-    //void managerLogin();
-    //void sunWindow();
-    /void monWindow();
-    void tueWindow();
-    void wedWindow();
-    void thuWindow();
-    void friWindow();
-    void satWindow();
-    void combinationUtil(int arr[],int data[],int start,int end,int index, int r);
-    void printCombinatin (int arr[], int n, int r);*/
+
 
     QMenu *file;
     QMenu *help;
@@ -64,52 +47,8 @@ private slots:
 
 
 
-   QFormLayout *main_layout ;
-   QLabel *managerLabel;
+
    QLabel *infoLabel;
-   QPushButton *managerBtn ;
-   QLabel *label;
-   QLineEdit *msg;
-   QLineEdit *name;
-   QLineEdit *status;
-   QLabel *time;
-   QLineEdit *mobileNo;
-   QLineEdit *username;
-   QLineEdit *password;
-   QLineEdit *tourName;
-   QLineEdit *tourTeam;
-   QLineEdit *tourGroup;
-   QLineEdit *tourGoal;
-   QLineEdit *homeGoal;
-   QLineEdit *awayGoal;
-   QLabel *homeTeamNo[120];
-   QLabel *awayTeamNo[120];
-   QLabel *homeScore[120];
-   QLabel *awayScore[120];
-
-   QLineEdit* tourDetail;
-   QLineEdit *teamName[50];
-   QLabel* teamnameLabel[50];
-   QLabel* nameLabel[50];
-   QLabel* homeLabel[50];
-   QLabel* awayLabel[50];
-   QLabel* versusLabel[50];
-   QGroupBox *groupBox[10];
-   QGridLayout *vbox[10];
-   QHBoxLayout *loginLayout [3];
-   QHBoxLayout *grpbox[10];
-
-   QLabel*msgRec;
-   QLabel *msgLabel;
-   QPushButton *updateResult[120];
-   int teamNo;
-   int groups;
-   int teams;
-   std::string file5;
-   std::string file6;
-
-   QLabel *fixtureLabel[120];
-   QLineEdit *fixtureResultNo;
 
 };
 
