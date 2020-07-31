c752f800b506fd55fa534a1850de2fae5ea4aa11
ayush7aryal <ayush7aryal@gmail.com>
2020-07-31 12:36:54 +0545

    faq completed

--- menu.cpp
+++ menu.cpp
@@ -75,25 +75,61 @@ void MainWindow::question()
     QLabel *ans1 = new QLabel();
     QLabel *q2 = new QLabel();
     QLabel *ans2 = new QLabel();
+    QLabel *q3 = new QLabel();
+    QLabel *ans3 = new QLabel();
 
-    QFont banner("Helvetica",30,QFont::Bold);
-    QFont question("Helvetica",20,QFont::Bold);
-    QFont ans("Helvetica",15,QFont::Bold);
+
+    //setting font for the questions and answers
+    QFont banner("Helvetica",30,QFont::ExtraBold);
+    QFont question("Helvetica",20,QFont::Black);
+    QFont ans("Helvetica",15,QFont::ExtraLight);
 
    faq -> setAlignment(Qt::AlignCenter);
-   // faq -> setAlignment(Qt::AlignLeft);
-    faq ->setText(tr("FAQ" ));
-     faq->setFont(banner);
+   faq ->setText(tr("FAQ" ));
+   faq->setFont(banner);
+   faq->setFixedHeight(50);
 
 
    q1-> setAlignment(Qt::AlignLeft);
-   q1->setText(tr("Q1) Why this Hotel?"));
+  // q1->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+   q1->setText(tr("Q1) What is this software used for?"));
    q1->setFont(question);
+   q1->setFixedHeight(50);
 
    ans1-> setAlignment(Qt::AlignLeft);
-   ans1->setText(tr("Because this is the best"));
+   //ans1->setFrameStyle(QFrame::Panel );
+   ans1->setFixedHeight(70);
+   ans1->setText("Ans) This hotel management software is used to keep all the recording of the datas of the costumers and enables to the smooth running of \n"
+                    "the hotel. It is basically used to digitize the record keeping of the hotel.");
    ans1->setFont(ans);
 
+
+   q2-> setAlignment(Qt::AlignLeft);
+  // q1->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+   q2->setText(tr("Q2) How do I login to the software?"));
+   q2->setFont(question);
+   q2->setFixedHeight(50);
+
+
+
+   ans2-> setAlignment(Qt::AlignLeft);
+   //ans1->setFrameStyle(QFrame::Panel );
+   ans2->setFixedHeight(100);
+   ans2->setText("Ans) To login to the software you need to be the employee of the hotel and you need to have your login id and password provided to\n you by the hotel.");
+   ans2->setFont(ans);
+
+
+   q3-> setAlignment(Qt::AlignLeft);
+  // q1->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+   q3->setText(tr("Q3) How do I keep the datas of the guest?"));
+   q3->setFont(question);
+   q3->setFixedHeight(50);
+
+   ans3-> setAlignment(Qt::AlignLeft);
+   //ans1->setFrameStyle(QFrame::Panel );
+   ans3->setFixedHeight(100);
+   ans3->setText("Ans) To input the datas of the guest, click on the rooms button and then select the preferred room by thr guest them a form pops up\n in the screen and then fill the information of the guest");
+   ans3->setFont(ans);
 
 
 
@@ -105,6 +141,8 @@ void MainWindow::question()
     layout->addWidget(ans1);
     layout->addWidget(q2);
     layout->addWidget(ans2);
+    layout->addWidget(q3);
+    layout->addWidget(ans3);
 
 
 
