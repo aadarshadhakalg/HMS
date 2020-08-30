877e444f3740de3cdd0c59d2ba07b7d3391bb754
sushant-c <sushantadhikari2001@gmail.com>
2020-08-30 13:43:29 +0545

    More changes

--- room.cpp
+++ room.cpp
@@ -1,55 +1,87 @@
-#include "room.h"
 #include "mainwindow.h"
-#include <iostream>
+void MainWindow::Room(){
+    setWindowTitle("Room");
+    style();
 
-Room::Room()
-{
+    QLabel *label = new QLabel();
+    label->setText("Common");
+    QFont banner("Helvetica",20,QFont::Bold);
+    label->setFont(banner);
+    label->setFixedHeight(90);
+
+    QLabel *label2 = new QLabel();
+    label2->setText("Deluxe");
+    label2->setFont(banner);
+    label2->setFixedHeight(90);
 
 
-}
+    rightside->setStyleSheet("*{background:white"
+                             ";}");
+    QString StyleSheetBtns =   "QPushButton { color: white; background-color: #1c2c3c; border: solid 5px white; font: 16pt 'Microsoft YaHei UI Light'; font-weight:bold; outline: none; } QPushButton:hover { background-color: #dc2525; border-style: solid; border-width: 3px; border-color: white; } QPushButton:pressed { background-color: #81DAF5; border-style: solid; border-width: 3px; border-color: #A9E2F3; }";
+
+    QPushButton* roomBtn = new QPushButton("Room");
+    QPushButton* PackageBtn = new QPushButton("Packages");
+    QPushButton* billBtn = new QPushButton("Billings");
+    QPushButton* customerBtn = new QPushButton("Records");
+    QPushButton* billBtn1 = new QPushButton("Billings");
+    QPushButton* billBtn2 = new QPushButton("Billings");
+    QPushButton* billBtn3 = new QPushButton("Billings");
+    QPushButton* bookbtn = new QPushButton("Book Now");
+    //Add button
+    QPushButton* addbtn = new QPushButton("Add Room");
+
+    roomBtn->setStyleSheet(StyleSheetBtns);
+    PackageBtn->setStyleSheet(StyleSheetBtns);
+    billBtn->setStyleSheet(StyleSheetBtns);
+    customerBtn->setStyleSheet(StyleSheetBtns);
+    billBtn1->setStyleSheet(StyleSheetBtns);
+    billBtn2->setStyleSheet(StyleSheetBtns);
+    billBtn3->setStyleSheet(StyleSheetBtns);
+    addbtn->setStyleSheet(StyleSheetBtns);
+    bookbtn->setStyleSheet(StyleSheetBtns);
 
 
-void Room::Checkin()
-{
 
-}
+    roomBtn->setFixedSize(200,120);
+    PackageBtn->setFixedSize(200,120);
+    billBtn->setFixedSize(200,120);
+    customerBtn->setFixedSize(200,120);
+    billBtn1->setFixedSize(200,120);
+    billBtn2->setFixedSize(200,120);
+    billBtn3->setFixedSize(200,120);
+    addbtn->setFixedSize(200,120);
+    bookbtn->setFixedSize(200,120);
 
-void Room::Checkout()
-{
 
-}
+    connect(roomBtn,SIGNAL(clicked()), this, SLOT(room1()));
+    connect(PackageBtn,SIGNAL(clicked()), this, SLOT(Package()));
+    connect(billBtn,SIGNAL(clicked()), this, SLOT(Bill()));
+    connect(customerBtn,SIGNAL(clicked()), this, SLOT(Customer()));
 
-bool Room::IsUnOccupied()
-{
-    return available;
+    QGridLayout *roomtype= new QGridLayout;
+    roomtype-> setHorizontalSpacing(30);
+     roomtype-> addWidget(label,0,0,1,4);
 
-}
+    roomtype-> addWidget(roomBtn,1,0,1,1);
+    roomtype-> addWidget(PackageBtn,1,1,1,1);
+    roomtype-> addWidget(billBtn1,1,2,1,1);
+    roomtype-> addWidget(billBtn2,1,3,1,1);
 
-QString Room::getRooms(){
-    return type;
-    return details;
-    QString room_price;
-    room_price =this->price;
-    return room_price;
+    roomtype-> addWidget(label2);
 
-}
-/*QString Room:: getName(){
-    return getguest.name();
-}*/
+    roomtype-> addWidget(billBtn,3,0,1,1);
+    roomtype-> addWidget(customerBtn,3,1,1,1);
+    roomtype-> addWidget(billBtn3,3,2,1,1);
+    roomtype-> addWidget(addbtn,4,0,1,1);
+    roomtype-> addWidget(bookbtn,4,3,1,1);
 
-QString Room:: getDetails() {
-    if(wifi)
-    {
 
-    }
-    if(tv)
-    {
 
-    }
 
-    if(couple_friendly)
-    {
+    rightside->setLayout(roomtype);
 
-    }
-    return details;
+    connect(bookbtn,SIGNAL(clicked()),this,SLOT(roombooking()));
+    connect(addbtn,SIGNAL(clicked()),this,SLOT(room_add()));
+
+
 }
