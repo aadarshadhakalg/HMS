740af8936246099c39d5f4bec2ed196e46b705a8
user-metaterongo <paudelayush007@gmail.com>
2020-09-24 19:50:15 +0545

    error in billing output

--- menu.cpp
+++ menu.cpp
@@ -53,13 +53,101 @@ void MainWindow::contact()
 
    setCentralWidget(widget);
 
-    infoLabel = new QLabel();
-    infoLabel -> setAlignment(Qt::AlignCenter);
-    infoLabel->setText(tr("Number k ho"));
+   QLabel *title = new QLabel();
+   QLabel *q1 = new QLabel();
+   QLabel *ans1 = new QLabel();
+   QLabel *q2 = new QLabel();
+   QLabel *ans2 = new QLabel();
+   QLabel *q3 = new QLabel();
+   QLabel *ans3 = new QLabel();
 
-    QVBoxLayout *layout = new QVBoxLayout;
 
-    layout->addWidget(infoLabel);
+
+
+
+
+
+   QFont banner("Helvetica",30,QFont::ExtraBold);
+   QFont question("Helvetica",20,QFont::Black);
+   QFont ans("Helvetica",15,QFont::ExtraLight);
+
+  title -> setAlignment(Qt::AlignCenter);
+  title ->setText(tr("CONTACT US" ));
+  title->setFont(banner);
+  //faq->setFixedHeight(40);
+  title->adjustSize();
+
+
+
+  q1-> setAlignment(Qt::AlignLeft);
+
+  q1->setText(tr("Contact Company"));
+  q1->setFont(question);
+  q1->adjustSize();
+
+  ans1-> setAlignment(Qt::AlignLeft);
+  //ans1->setFrameStyle(QFrame::Panel );
+  //ans1->setFixedHeight(100);
+  ans1->setText("This project is brought to you by student of Kathmandu University.\n"
+                   "You can contact us at : sanohotel@gmail.com.");
+  ans1->setFont(ans);
+  ans1->adjustSize();
+
+
+  q2-> setAlignment(Qt::AlignLeft);
+ // q1->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+  q2->setText(tr("About Developers"));
+  q2->setFont(question);
+  //q2->setFixedHeight(40);
+  q2->adjustSize();
+
+
+
+  ans2-> setAlignment(Qt::AlignLeft);
+  //ans1->setFrameStyle(QFrame::Panel );
+ // ans2->setFixedHeight(60);
+  ans2->setText("The developers of this application are\n"
+                "Aadarsha Dhakal \n"
+                "Ayush Paudel \n"
+                "Ayush Aryal \n"
+                "Sushant Adhikari.");
+  ans2->setFont(ans);
+  ans2->adjustSize();
+
+
+  q3-> setAlignment(Qt::AlignLeft);
+ // q1->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+  q3->setText(tr("HELP US GROW"));
+  q3->setFont(question);
+  //q3->setFixedHeight(40);
+  q3->adjustSize();
+
+  ans3-> setAlignment(Qt::AlignLeft);
+  //ans1->setFrameStyle(QFrame::Panel );
+  //ans3->setFixedHeight(60);
+  ans3->setText("You can help us by funding our work");
+  ans3->setFont(ans);
+  ans3->adjustSize();
+
+  QPushButton *backButton = new QPushButton("Back");
+  backButton->setStyleSheet("*{background:blue;height:30px;}");
+  backButton->adjustSize();
+  connect(backButton,SIGNAL(clicked()),this,SLOT(dashboard()));
+
+
+   QVBoxLayout *layout = new QVBoxLayout;
+
+
+   layout->addWidget(title);
+   layout->addWidget(q1);
+   layout->addWidget(ans1);
+   layout->addWidget(q2);
+   layout->addWidget(ans2);
+   layout->addWidget(q3);
+   layout->addWidget(ans3);
+   layout->addWidget(backButton);
+                                                                  // Navigates to the loginScreen.
+
     widget->setLayout(layout);
 
 }
