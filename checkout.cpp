8bf231535af559d19ff1b9eba8c892e5d62bea02
user-metaterongo <paudelayush007@gmail.com>
2020-09-24 19:26:58 +0545

    error in billing output

--- checkout.cpp
+++ checkout.cpp
@@ -10,6 +10,19 @@ void MainWindow::checkout(int room){
 
     QWidget *widget = new QWidget;
     setCentralWidget(widget);
+    QVBoxLayout *layout = new QVBoxLayout;
+
+
+    Database db;
+    Guest guest = db.getGuestDetailByRoomNo(room);
+    if(guest.getID() == NULL)
+    {   infoLabel = new QLabel();
+        infoLabel->setText("No user");
+    }
+    else{
+        infoLabel = new QLabel();
+        infoLabel->setText("Bill  For HMS");
+    }
     QGridLayout *user_formLayout = new QGridLayout(); // Defines grid layout for name
     user_formLayout->setColumnMinimumWidth(1,300);
 
@@ -40,46 +53,60 @@ void MainWindow::checkout(int room){
     nationalityHint->setStyleSheet("*{font-weight:bold;font-size:15px;padding:10px;}");
 
     // Input Forms
-    this->customer_name = new QLineEdit();    //this refers to the MainWindow class
-    this->customer_email = new QLineEdit();
-    this->customer_phone = new QLineEdit();
-    this->customer_address = new QLineEdit();
-    this->customer_nationality = new QLineEdit();
+    QLabel *custo_name = new QLabel();    //this refers to the MainWindow class
+    QLabel *custo_email = new QLabel();
+    QLabel *custo_phone = new QLabel();
+    QLabel *custo_address = new QLabel();
+    QLabel *custo_nationality = new QLabel();
+    QLabel *Total = new QLabel();
+    QLabel *Paid = new QLabel();
+    QLabel *Due = new QLabel();
 
-    //font style
-    customer_name->setFixedHeight(40);
-    customer_name->setClearButtonEnabled(true);
+    custo_name->setText(guest.getName());
+    custo_email->setText(guest.getEmail());
+    custo_phone->setText(guest.getContact());
+    custo_address->setText(guest.getAddress());
+    custo_nationality->setText(guest.getIdentity());
+    Total->setNum(guest.getTotalAmount());
+    Paid->setNum(guest.getTotalAmount());
+    Due->setNum(guest.getTotalAmount());
 
-    customer_email->setFixedHeight(40);
-    customer_email->setPlaceholderText("Active Email");
-    customer_email->setClearButtonEnabled(true);
 
-    customer_phone->setFixedHeight(40);
-    customer_phone->setClearButtonEnabled(true);
 
-    customer_address->setFixedHeight(40);
-    customer_address->setClearButtonEnabled(true);
-    customer_address->setPlaceholderText("Current Address");
 
-    customer_nationality->setFixedHeight(40);
-    customer_nationality->setClearButtonEnabled(true);
 
     //Adding widgets in the userForm layout
+    user_formLayout-> setHorizontalSpacing(30);
     user_formLayout->addWidget(nameHint,0,0);
-    user_formLayout->addWidget(customer_name,0,1);
+    user_formLayout->addWidget(custo_name,0,1);
 
     user_formLayout->addWidget(emailHint,1,0);
-    user_formLayout->addWidget(customer_email,1,1);
+    user_formLayout->addWidget(custo_email,1,1);
 
     user_formLayout->addWidget(phoneHint,2,0);
-    user_formLayout->addWidget(customer_phone,2,1);
+    user_formLayout->addWidget(custo_phone,2,1);
 
     user_formLayout->addWidget(addressHint,3,0);
-    user_formLayout->addWidget(customer_address,3,1);
+    user_formLayout->addWidget(custo_address,3,1);
 
     user_formLayout->addWidget(nationalityHint,4,0);
-    user_formLayout->addWidget(customer_nationality,4,1);
+    user_formLayout->addWidget(custo_nationality,4,1)
+            ;
+    user_formLayout->addWidget(TotalPayHint,4,1);
+    user_formLayout->addWidget(Total,4,1);
+
+    user_formLayout->addWidget(PaidPayHint,4,1);
+    user_formLayout->addWidget(Paid,4,1);
+
+    user_formLayout->addWidget(DuePayHint,4,1);
+    user_formLayout->addWidget(Due,4,1);
+
+
+
 
     user_formLayout->setColumnStretch(4,1);
+   layout->addWidget(infoLabel);
     widget->setLayout(user_formLayout);
+    widget->setLayout(layout);
+
 }
