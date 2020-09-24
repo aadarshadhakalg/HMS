6622e5d42e0efb0f37e1f0d480fcf76d2e90e48a
aadarshadhakalg <braveboy2058@gmail.com>
2020-09-24 20:46:58 +0545

    Fetch User Detail Class Bug Fixed

--- database.cpp
+++ database.cpp
@@ -91,6 +91,7 @@ Package Database::getPackageByID(int id){
     query.prepare("SELECT * FROM packages WHERE id=:id");
     query.bindValue(":id", id);
     if(query.exec()){
+            query.first();
             pkg.setName(query.value(1).toString());
             pkg.setCompany(query.value(2).toString());
             pkg.setDetails(query.value(3).toString());
@@ -123,6 +124,7 @@ Guest Database::getGuestDetailByRoomNo(int no){
     query.prepare("SELECT * FROM guests WHERE room_no=:no and status='Active'");
     query.bindValue(":no", no);
     if(query.exec()){
+        query.first();
         guest.setID(query.value(0).toInt());
         guest.setRoomNo(query.value(1).toInt());
         guest.setName(query.value(2).toString());
